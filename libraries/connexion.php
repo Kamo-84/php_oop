878fa22184433a18ff5ef01c2a8af78f0034154a
Kamo Yepremyan <yepremyan2018@gmail.com>
2021-10-28 19:45:45 +0200

    Create libraries folder and connexion.php file to centralise connection

---
+++ libraries/connexion.php
@@ -0,0 +1,10 @@
+<?php
+
+function getPdo(): PDO
+{
+  $pdo = new PDO('mysql:host=localhost:8889;dbname=blogpoo;charset=utf8', 'root', 'root', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ]);
+  return $pdo;
+}
